72185ac5b0bf8a8ac43042d1c54f5ac4e2596e15
maqiie <99504010+maqiie@users.noreply.github.com>
2023-09-27 09:52:30 +0300

    updates

--- src/components/Blogs/PostDetail.js
+++ src/components/Blogs/PostDetail.js
@@ -1,16 +1,3 @@
-import React from "react";
-
-
-
-const PostDetail = ({ post }) => {
-  return (
-    <div className="post-detail">
-      <h2 className="post-title">{post.title}</h2>
-      <p className="post-author">By {post.author}</p>
-      <div className="post-content">{post.content}</div>
-      {/* Add any other details you want to display */}
-    </div>
-  );
-};
-
-export default PostDetail;
+version https://git-lfs.github.com/spec/v1
+oid sha256:5034451f9faf8e4e837a62359c12697ac4b189f2728a4358e7fbe117a51c4e85
+size 375
